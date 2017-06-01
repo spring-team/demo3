331286396f8807bdc60fb75fe6f123aa69016950
Christian Dupuis <cd@atomist.com>
2017-06-02 00:41:42 +0200

    Update HomeController.java

--- src/main/java/com/atomist/springteam/HomeController.java
+++ src/main/java/com/atomist/springteam/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "Hello REST Microservice World";
     }
+    
+    @RequestMapping(path = "/hello")
+    public String hello() {
+        return "Hello REST Microservice World";
+    }
 }
